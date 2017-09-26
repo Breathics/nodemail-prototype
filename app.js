4a6c9619dccd6394b1e3d927af307168435c1cca
Andy Ong <aong93@gmail.com>
2017-09-26 14:07:03 -0700

    removed sensitive data

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 // Require modules
 const nodemailer = require('nodemailer');
+const { USERNAME, PASSWORD } = require('./config.js');
 
-// If you're going to use your own email
-// You may have to allow less secure apps
+// If you're going to use your own gmail
+// You have to allow less secure apps
 // My Account >> Sign-in & Security >> Connected apps & sites >> Allow less secure apps: ON
 
 // You're free to use whatever service you would like
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
   auth: {
-    user: 'YOUR EMAIL HERE',     
-    pass: 'YOUR PASSWORD HERE'  
+    user: USERNAME,     
+    pass: PASSWORD  
   }                             
 });
 
-
+// Four important options for our mailOptions
 const mailOptions = {
-  from: 'EMAIL COMING FROM AUTH', // May not even need to include it here
-  to: 'EMAIL SENDING TO',
-  subject: 'It works',
-  text: 'Sup doood'
+  from: 'breathics@gmail.com',         // Sender of the email
+  to: 'andy.ong@learningfuze.com',  // Recipient of the email
+  subject: 'It works',              // Subject of the email
+  text: 'Sup doood',                // Message of the email
+  // html: '<h1>SUP DOOOD</h1>'     // Can be used in place of the text
 };
 
 transporter.sendMail(mailOptions, (error, info) => {
@@ -29,4 +32,3 @@ transporter.sendMail(mailOptions, (error, info) => {
     console.log('Email sent successfully' + info.response);
   }
 });
-
