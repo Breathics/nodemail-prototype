50a71991ba70de65758eaf9bc66d5cf36d8a10fc
Andy Ong <aong93@gmail.com>
2017-09-26 12:45:37 -0700

    included route and consumed middleware

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const { USERNAME, PASSWORD } = require('./config.js');
 
+const app = express();
+const PORT = 8000;
+
+
+// Consume middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
 // If you're going to use your own gmail
 // You have to allow less secure apps
 // My Account >> Sign-in & Security >> Connected apps & sites >> Allow less secure apps: ON
@@ -17,20 +26,30 @@ const transporter = nodemailer.createTransport({
   }                             
 });
 
-// Four important options for our mailOptions
-const mailOptions = {
-  from: 'aong93@gmail.com',         // Sender of the email
-  to: 'andy.ong@learningfuze.com',  // Recipient of the email
-  subject: 'It works',              // Subject of the email
-  text: 'Sup doood',                // Message of the email
-  // html: '<h1>SUP DOOOD</h1>'     // Can be used in place of the text
-};
+// HTTP POST route to accept POST data from 
+app.post('/email', (req, res) => {
+  // Four important options for our mailOptions
+  const mailOptions = {
+    from: 'breathics@gmail.com',         // Sender of the email
+    to: 'andy.ong@learningfuze.com',  // Recipient of the email
+    subject: 'It works',              // Subject of the email
+    text: 'Sup doood',                // Message of the email
+    // html: '<h1>SUP DOOOD</h1>'     // Can be used in place of the text
+  };
 
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent successfully' + info.response);
-  }
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent successfully' + info.response);
+    }
+  });
+
 });
+
+
+
+app.listen(PORT, function(req ,res){
+  console.log("Yo, we're cooking up something hot on PORT: ", PORT);
+})
 
