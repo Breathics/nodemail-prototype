9d1bdead9a325f4b1e6679afc90f79253fed5d5f
Andy Ong <aong93@gmail.com>
2017-09-26 11:21:48 -0700

    included more comments on each piece of the nodeMailer protottype, ignoring config.js for security purposes

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ```
 1. npm install
 2. Configure objects
+    - config.js
     - transporter
     - mailOptions
 3. npm start
--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 // Require modules
 const nodemailer = require('nodemailer');
+const { USERNAME, PASSWORD } = require('./config.js');
 
-// If you're going to use your own email
-// You may have to allow less secure apps
+// If you're going to use your own gmail
+// You have to allow less secure apps
 // My Account >> Sign-in & Security >> Connected apps & sites >> Allow less secure apps: ON
 
 // You're free to use whatever service you would like
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'YOUR EMAIL HERE',     
-    pass: 'YOUR PASSWORD HERE'  
+    user: USERNAME,     
+    pass: PASSWORD  
   }                             
 });
 
-
+// Four important options for our mailOptions
 const mailOptions = {
-  from: 'EMAIL COMING FROM AUTH', // May not even need to include it here
-  to: 'EMAIL SENDING TO',
-  subject: 'It works',
-  text: 'Sup doood'
+  from: 'aong93@gmail.com',         // Sender of the email
+  to: 'andy.ong@learningfuze.com',  // Recipient of the email
+  subject: 'It works',              // Subject of the email
+  text: 'Sup doood',                // Message of the email
+  // html: '<h1>SUP DOOOD</h1>'     // Can be used in place of the text
 };
 
 transporter.sendMail(mailOptions, (error, info) => {
